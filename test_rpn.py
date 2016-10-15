99602ac2d7a29618535d8273a464fb3531e50934
wipatric <wipatric@umich.edu>
2016-10-15 14:47:33 -0400

    added exponentiation test function

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,6 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test_exponentiation(self):
+		result = rpn.calculate("4 3 ^")
+		self.assertEqual(64, result)
