757edfd2e2abc93dd46072515eede39c65e13305
wipatric <wipatric@umich.edu>
2016-10-15 14:32:57 -0400

    downloaded the final version from website

--- rpn.py
+++ rpn.py
@@ -2,32 +2,35 @@
 
 import operator
 
-operators = {
-        '+': operator.add,
-        '-': operator.sub,
-        '*': operator.mul,
-        '/': operator.truediv,
-      } 
 
-def calculate(myarg1):
-        stack = list()
-        for token in myarg1.split():
-                try:
-                        stack.append(int(token))
-                except ValueError:
-                        arg2 = stack.pop()
-                        arg1 = stack.pop()
-                        function = operators[token]
-                        result = function(arg1, arg2)
-                        stack.append(result)
-        if len(stack) != 1:
-                raise TypeError
-        print(stack)
-        return stack.pop()
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
+def calculate(myarg):
+	stack = list()
+	for token in myarg.split():
+		try:
+			token = int(token)
+			stack.append(token)
+		except ValueError:
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Too many parameters")
+	return stack.pop()
 
 def main():
-        while True:
-                calculate(input("rpn calc> "))
+	while True:
+		result = calculate(input("rpn calc> "))
+		print("Result: ", result)
 
 if __name__ == '__main__':
 	main()
